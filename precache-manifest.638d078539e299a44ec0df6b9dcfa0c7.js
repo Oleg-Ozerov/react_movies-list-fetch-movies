7680eee75d41260c42efb37d8c1fa73cdde9175e
Oleg <mvp.westbrook4k@gmail.com>
2020-09-16 13:36:03 +0300

    Updates

---
+++ precache-manifest.638d078539e299a44ec0df6b9dcfa0c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc846f5c78ea815c5562b16e9d989575",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bef337ee2440f09ca59f",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "445e0d269adc650faf41",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "bef337ee2440f09ca59f",
+    "url": "./static/js/2.11e02830.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.11e02830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "445e0d269adc650faf41",
+    "url": "./static/js/main.aadcae01.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
